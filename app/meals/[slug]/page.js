b29f0e0680fe0f412edb1da0ae599b63064827ef
Natalia <natalia21080@yahoo.de>
2024-12-05 13:59:17 +0100

    create slug.js

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,37 @@
-export default function MealDetails(){
+import classes from "./page.module.css"
+import Image from "next/image";
+import {getMeal} from "@/lib/meals";
+import {notFound} from "next/navigation";
+
+export default function MealDetails({params}) {
+
+    const meal = getMeal(params.slug)
+    meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+
+    if (!meal) {
+        notFound();
+    }
+
     return (
         <>
-            <h1> Meal Details</h1>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill/>
+                </div>
+                <div className={classes.headerText}>
+                    <h1>TITLE</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{__html: meal.instructions}}>
+                </p>
+            </main>
+
+
         </>
     )
 }
